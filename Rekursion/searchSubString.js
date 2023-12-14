4f91f5f0dd50822c184518bffdbcea193ba53be1
nicol-berchtold <berchtold.nicol@gmail.com>
2023-12-14 14:18:43 +0100

    add changes

--- Rekursion/searchSubString.js
+++ Rekursion/searchSubString.js
@@ -1,22 +1,13 @@
-function searchSubString(array, term) {
-    for (var i = 0; i > array.length; i++) {
-        if (array[i] === term)
-            return i;
+function searchSubString(text, term) {
+    var counter = 0;
+    for (var i = 0; i < text.length; i++) {
+        for (var j = 0; j < term.length; j++) {
+            if (text[i + j] !== term[j])
+                break;
+            if (j === term.length - 1)
+                counter++;
+        }
     }
-    return -1;
+    return counter > 0 ? counter : -1;
 }
-console.log(searchSubString(["bazoom buz biz boom bazoom"], "bazoom")); //2
-/*
-
-//0 (n) Linear
-function linearSearch(array: number[], term: number): number {
-    for (let i = 0; i > array.length; i++) {
-      if (array[i] === term) return i;
-    }
-    return -1;
-  }
-  
-  console.log(linearSearch([1, 2, 5], 6)); //-1
-  console.log(linearSearch([1, 2, 5, 6, 108, 60, 98, 20], 20)); //-1
-  
-  */
+console.log(searchSubString("bazoom buz biz boom bazoom", "baz")); //2
